b7e59ceedd245e7788aabf3bdfc346cb1ee1a7d9
Jagruti Hota <jagrutihota92@gmail.com>
2024-09-10 00:23:14 +0530

    create repo

---
+++ src/layouts/public/Navbar.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <nav className="w-full h-fit p-5 sticky top-0">
+        <div className="w-full h-10 bg-red-500">
+
+        </div>
+      
+    </nav>
+  )
+}
+
+export default Navbar
---
+++ src/layouts/public/index.jsx
@@ -0,0 +1,38 @@
+// import { useAppContext } from "contexts";
+import Head from "next/head";
+import Navbar from "./Navbar";
+
+const PublicLayout = ({
+  children = <></>,
+  title = "Shree Odisha Handloom",
+  description,
+  ogImage,
+}) => {
+  return (
+    <>
+      <Head>
+        <meta
+          property="og:url"
+          content=""
+        />
+        <meta property="og:type" content="website" />
+        <title>{title}</title>
+        <meta
+          name="description"
+          content=""
+        />
+        <meta
+          property="og:image"
+          content={
+            ogImage ? ogImage : ""
+          }
+        />
+      </Head>
+      <main className=" w-full relative ">
+        <Navbar />
+        {children}
+      </main>
+    </>
+  );
+};
+export default PublicLayout;
