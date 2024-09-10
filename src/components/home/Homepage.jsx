965d6f2e50c9909661fa731f044ab92f839a7fc6
Jagruti Hota <jagrutihota92@gmail.com>
2024-09-10 21:00:09 +0530

    new

--- src/components/home/Homepage.jsx
+++ src/components/home/Homepage.jsx
@@ -1,7 +1,7 @@
 const Homepage = () => {
   return (
     <div className="relative bg-[url('/home/3.jpg')] bg-no-repeat bg-center bg-cover h-screen">
-      {/* Main Content */}
+    
       <div className="relative z-10 flex flex-col items-start justify-center h-full p-6 md:p-16">
         <h1 className="text-6xl font-bold text-white">Ishita Vishwakarma</h1>
         <p className="mt-4 text-lg text-white max-w-lg">
