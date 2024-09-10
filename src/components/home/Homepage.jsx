7eddfd45a62477d6b2d8078f2490c224e45fcc0a
Jagruti Hota <jagrutihota92@gmail.com>
2024-09-10 23:36:35 +0530

    new changes

--- src/components/home/Homepage.jsx
+++ src/components/home/Homepage.jsx
@@ -3,11 +3,17 @@ const Homepage = () => {
     <div className="relative bg-[url('/home/3.jpg')] bg-no-repeat bg-center bg-cover h-screen">
     
       <div className="relative z-10 flex flex-col items-start justify-center h-full p-6 md:p-16">
-        <h1 className="text-6xl font-bold text-white">Ishita Vishwakarma</h1>
-        <p className="mt-4 text-lg text-white max-w-lg">
+        <h1 className="text-6xl font-semibold text-white">Ishita</h1>
+        <h1 className="text-6xl font-medium text-white"> Vishwakarma</h1>
+
+        <p className="mt-4 text-lg text-gray-300 max-w-lg">
        
-          Winner Of <span className="font-semibold">Sa Re Ga Ma Pa</span> | 1st Runner Up <span className="font-semibold">`India's Got Talent`</span>
+          Winner Of <span className="font-semibold !text-white  ">Sa Re Ga Ma Pa</span> | 1st Runner Up <span className="font-semibold !text-white ">India's Got Talent</span>
         </p>
+      </div>
+
+      <div>
+        {/* todo section */}
       </div>
     </div>
   );
--- src/layouts/public/Navbar.jsx
+++ src/layouts/public/Navbar.jsx
@@ -1,36 +1,57 @@
-import Link from 'next/link'
-import React from 'react'
+import Link from 'next/link';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faFacebookF, faInstagram, faTwitter, faLinkedin, faYoutube } from '@fortawesome/free-brands-svg-icons';
 
 const Navbar = () => {
   return (
-    <nav className="w-full h-fit p-5 fixed top-0 bg-[linear-gradient(to_left,_rgba(10,10,10,0.1),_rgba(10,10,10,0.1))] rounded-lg m-2">
-      <div className="w-full flex items-center justify-between gap-5">
-          {/* Logo */}
-          <div className="text-white font-bold text-2xl">
-            <span className="font-bold text-white">I</span>
-            <span className="text-white">ISHITA</span> MUSIC
-          </div>
+    <nav className="w-full  fixed top-0 p-6  bg-gradient-to-r from-black/40 to-black/40 rounded-lg shadow-md z-50 m-4 ">
+      <div className="flex items-center justify-between">
+        {/* Logo */}
+        <div className="text-white font-bold text-2xl">
+          <span className="text-white">I</span>
+          <span className="text-white">SHITA</span> MUSIC
+        </div>
 
-          {/* Navigation Links */}
-          <div className="hidden md:flex space-x-8 text-white text-lg">
-            <Link href="/" className="hover:underline">Home</Link>
-            <Link href="" className="hover:underline">About</Link>
-            <Link href="#" className="hover:underline">Music</Link>
-            <Link href="#" className="hover:underline">Press</Link>
-            <Link href="#" className="hover:underline">Contact</Link>
-          </div>
+        {/* Navigation Links */}
+        <div className="hidden md:flex space-x-8 text-white text-lg">
+          <Link href="/" className="hover:underline">
+            Home
+          </Link>
+          <Link href="#" className="hover:underline">
+            About
+          </Link>
+          <Link href="#" className="hover:underline">
+            Music
+          </Link>
+          <Link href="#" className="hover:underline">
+            Press
+          </Link>
+          <Link href="#" className="hover:underline">
+            Contact
+          </Link>
         </div>
 
         {/* Social Icons */}
         <div className="hidden md:flex space-x-4 text-white">
-          <Link href="#"><i className="fab fa-facebook-f"></i></Link>
-          <Link href="#"><i className="fab fa-instagram"></i></Link>
-          <Link href="#"><i className="fab fa-twitter"></i></Link>
-          <Link href="#"><i className="fab fa-linkedin"></i></Link>
-          <Link href="#"><i className="fab fa-youtube"></i></Link>
+          <Link href="#">
+            <FontAwesomeIcon icon={faFacebookF} />
+          </Link>
+          <Link href="#">
+            <FontAwesomeIcon icon={faInstagram} />
+          </Link>
+          <Link href="#">
+            <FontAwesomeIcon icon={faTwitter} />
+          </Link>
+          <Link href="#">
+            <FontAwesomeIcon icon={faLinkedin} />
+          </Link>
+          <Link href="#">
+            <FontAwesomeIcon icon={faYoutube} />
+          </Link>
         </div>
+      </div>
     </nav>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
