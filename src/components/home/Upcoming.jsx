dff9f86e2898ea92def85f56900ddf510dc5854b
IamVarun18 <130641848+IamVarun18@users.noreply.github.com>
2024-09-16 22:52:37 +0530

    home complete

--- src/components/home/Upcoming.jsx
+++ src/components/home/Upcoming.jsx
@@ -1,14 +1,23 @@
-import React from 'react'
+import React from 'react';
+import { BiSolidDownArrow } from "react-icons/bi";
 
 const Upcoming = () => {
     return (
-        <div className='h-screen bg-green-400 flex justify-center '>
-            <div >
-                <h1 className='text-center text-9xl font-light'>Recent & Upcoming <br />Realises</h1>
-
+        <div className='h-screen bg-green-300 flex  relative justify-center items-center'>
+            <div className='flex flex-col gap-y-14 justify-center items-center'>
+                <h1 className='text-center leading-normal text-8xl font-light'>
+                    Recent & Upcoming <br /> Releases <br /> |
+                </h1>
+                <button className='rounded px-14 py-2 p-10 bg-white flex items-center gap-x-2'>
+                    <h2 className='text-2xl font-medium'>View All</h2>
+                   
+                </button>
+                <div className='absolute bottom-10'>
+                <BiSolidDownArrow size={44} />
+            </div>
             </div>
         </div>
-    )
+    );
 }
 
-export default Upcoming
+export default Upcoming;
