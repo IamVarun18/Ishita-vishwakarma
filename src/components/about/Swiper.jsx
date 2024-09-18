cd68536c3a003f07666b42e0775fe3983a73d0cf
IamVarun18 <130641848+IamVarun18@users.noreply.github.com>
2024-09-19 00:08:27 +0530

    slide 1

--- src/components/about/Swiper.jsx
+++ src/components/about/Swiper.jsx
@@ -10,33 +10,33 @@ import 'swiper/css/effect-creative';
 import { EffectCreative } from 'swiper/modules';
 
 const Swiper = () => {
-    return (
-        <SwiperReact
-            grabCursor={true}
-            effect={'creative'}
-            creativeEffect={{
-                prev: {
-                    shadow: true,
-                    translate: [0, 0, -400],
-                },
-                next: {
-                    translate: ['100%', 0, 0],
-                },
-            }}
-            modules={[EffectCreative]}
-            className="mySwiper"
-        >
-            <SwiperSlide className=''><img src="https://images.unsplash.com/photo-1726597764489-5ba1c00a178a?q=80&w=1965&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="" /></SwiperSlide>
-            <SwiperSlide>Slide 2</SwiperSlide>
-            <SwiperSlide>Slide 3</SwiperSlide>
-            <SwiperSlide>Slide 4</SwiperSlide>
-            <SwiperSlide>Slide 5</SwiperSlide>
-            <SwiperSlide>Slide 6</SwiperSlide>
-            <SwiperSlide>Slide 7</SwiperSlide>
-            <SwiperSlide>Slide 8</SwiperSlide>
-            <SwiperSlide>Slide 9</SwiperSlide>
-        </SwiperReact>
-    );
+  return (
+    <SwiperReact
+      grabCursor={true}
+      effect={'creative'}
+      creativeEffect={{
+        prev: {
+          shadow: true,
+          translate: [0, 0, -400],
+        },
+        next: {
+          translate: ['100%', 0, 0],
+        },
+      }}
+      modules={[EffectCreative]}
+      className="mySwiper"
+    >
+      <SwiperSlide className=''>Slide 1</SwiperSlide>
+      <SwiperSlide>Slide 2</SwiperSlide>
+      <SwiperSlide>Slide 3</SwiperSlide>
+      <SwiperSlide>Slide 4</SwiperSlide>
+      <SwiperSlide>Slide 5</SwiperSlide>
+      <SwiperSlide>Slide 6</SwiperSlide>
+      <SwiperSlide>Slide 7</SwiperSlide>
+      <SwiperSlide>Slide 8</SwiperSlide>
+      <SwiperSlide>Slide 9</SwiperSlide>
+    </SwiperReact>
+  );
 };
 
 export default Swiper;
