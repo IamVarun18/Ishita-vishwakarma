1c8a3c2592797b81f2c0276593b5aaeb047641c9
IamVarun18 <130641848+IamVarun18@users.noreply.github.com>
2025-01-08 23:00:51 +0530

    new changes

--- src/components/music/Allmusic.jsx
+++ src/components/music/Allmusic.jsx
@@ -1,5 +1,4 @@
 import React, { useState } from 'react';
-import Image from 'next/image'; // Import Image component
 
 const AllMusic = () => {
   const songs = [
@@ -24,7 +23,134 @@ const AllMusic = () => {
         }
       }
     },
-    // Add other songs here...
+    {
+      title: 'Prabhu Govardhan Girdhari',
+      artist: 'Ft. Ishita Viswakarma',
+      duration: '6:41',
+      releaseDate: '08/11/23',
+      category: 'Devotional',
+      platforms: {
+        spotify: {
+          icon: '/ICONS/Icons/1x/spotify.png',
+          url: 'https://open.spotify.com/track/4ftko0dTn32lYxtVcQUhV4?si=28fc6646584346a2'
+        },
+        appleMusic: {
+          icon: '/ICONS/Icons/1x/imusic.png',
+          url: 'https://music.apple.com/in/album/prabhu-govardhan-girdhari-single/1715642027'
+        },
+        youtube: {
+          icon: '/ICONS/Icons/1x/ytcolor.png',
+          url: 'https://www.youtube.com/embed/tumbfI7scCU?si=-ULg-5llBdqGYVRM'
+        }
+      }
+    },
+    {
+      title: 'Hanuman Chalisa',
+      artist: 'Ft. Ishita Viswakarma',
+      duration: '6:41',
+      releaseDate: '06/04/22',
+      category: 'Devotional',
+      platforms: {
+        spotify: {
+          icon: '/ICONS/Icons/1x/spotify.png',
+          url: 'https://open.spotify.com/artist/2oaus66w5EZnNJo22QGq8Z?si=iuLQCNZRTsaHEjccN3xGDw'
+        },
+        appleMusic: {
+          icon: '/ICONS/Icons/1x/imusic.png',
+          url: 'https://www.apple.com/in/search/ishita-vishwakarma?src=globalnav'
+        },
+        youtube: {
+          icon: '/ICONS/Icons/1x/ytcolor.png',
+          url: 'https://www.youtube.com/embed/Qya5cZBZNRI?si=oQ8-VfC-TONhozQi'
+        }
+      }
+    },
+    
+    {
+      title: 'Ab Navyug Ki Gangotri Se ( PragyaGeet )',
+      artist: 'Ft. Ishita Viswakarma',
+      duration: '6:59',
+      releaseDate: '29/07/23',
+      category: 'Devotional',
+      platforms: {
+        spotify: {
+          icon: '/ICONS/Icons/1x/spotify.png',
+          url: 'https://open.spotify.com/artist/2oaus66w5EZnNJo22QGq8Z?si=iuLQCNZRTsaHEjccN3xGDw'
+        },
+        appleMusic: {
+          icon: '/ICONS/Icons/1x/imusic.png',
+          url: 'https://www.apple.com/in/search/ishita-vishwakarma?src=globalnav'
+        },
+        youtube: {
+          icon: '/ICONS/Icons/1x/ytcolor.png',
+          url: "https://www.youtube.com/embed/xo88d4ZhnSg?si=8X4hj2RrGgHVzqXK"
+        }
+      }
+    },
+    {
+      title: 'Ram Ke Aane Se | Ayodhya Ram Mandir Anthem',
+      artist: 'Ft. | Samarpit Golani | Ishita Vishwakarma',
+      duration: '3:16',
+      releaseDate: '18/01/24',
+      category: 'Devotional',
+      platforms: {
+        spotify: {
+          icon: '/ICONS/Icons/1x/spotify.png',
+          url: 'https://open.spotify.com/artist/2oaus66w5EZnNJo22QGq8Z?si=iuLQCNZRTsaHEjccN3xGDw'
+        },
+        appleMusic: {
+          icon: '/ICONS/Icons/1x/imusic.png',
+          url: 'https://www.apple.com/in/search/ishita-vishwakarma?src=globalnav'
+        },
+        youtube: {
+          icon: '/ICONS/Icons/1x/ytcolor.png',
+          url: "https://www.youtube.com/embed/AdX19OvxAC0?si=BR25ud3IaF2xBvQ_"
+        }
+      }
+    },{
+      title: 'Ab Navyug Ki Gangotri Se ( PragyaGeet )',
+      artist: 'Ft. Ishita Viswakarma',
+      duration: '6:59',
+      releaseDate: '29/07/23',
+      category: 'Devotional',
+      platforms: {
+        spotify: {
+          icon: '/ICONS/Icons/1x/spotify.png',
+          url: 'https://open.spotify.com/artist/2oaus66w5EZnNJo22QGq8Z?si=iuLQCNZRTsaHEjccN3xGDw'
+        },
+        appleMusic: {
+          icon: '/ICONS/Icons/1x/imusic.png',
+          url: 'https://www.apple.com/in/search/ishita-vishwakarma?src=globalnav'
+        },
+        youtube: {
+          icon: '/ICONS/Icons/1x/ytcolor.png',
+          url: "https://www.youtube.com/embed/xo88d4ZhnSg?si=8X4hj2RrGgHVzqXK"
+        }
+      }
+    },
+
+    {
+      title: 'Play Back Symphony',
+      artist: 'John Doe',
+      duration: '4:20',
+      releaseDate: '01/12/23',
+      category: 'Play Back',
+      platforms: {
+        spotify: {
+          icon: '/ICONS/Icons/1x/spotify.png',
+          url: 'https://open.spotify.com/track/example2'
+        },
+        appleMusic: {
+          icon: '/ICONS/Icons/1x/imusic.png',
+          url: 'https://music.apple.com/example2'
+        },
+        youtube: {
+          icon: '/ICONS/Icons/1x/ytcolor.png',
+          url: 'https://youtube.com/example2',
+          
+        }
+      }
+    }
   ];
 
   const [filter, setFilter] = useState('All Music');
@@ -36,10 +162,10 @@ const AllMusic = () => {
 
   return (
     <div className="min-h-screen bg-white flex justify-center items-start mt-16 py-16">
-      <div className="w-full lg:max-w-[80vw] max-w-[90vw]">
+      <div className="w-full lg:max-w-[80vw]  max-w-[90vw] ">
         {/* Filter Buttons */}
-        <div className="mb-10">
-          <div className="lg:flex gap-8 lg:w-auto lg:items-start lg:justify-between grid">
+        <div className="mb-10 ">
+          <div className="lg:flex gap-8 lg:w-auto   lg:items-start lg:justify-between grid">
             {categories.map(category => (
               <button
                 key={category}
@@ -73,12 +199,10 @@ const AllMusic = () => {
                     target="_blank"
                     rel="noopener noreferrer"
                   >
-                    <Image
+                    <img
                       src={icon}
                       alt={platform}
-                      width={24} // Specify appropriate width
-                      height={24} // Specify appropriate height
-                      className="lg:w-6 lg:h-6"
+                      className="w-8 h-8 lg:w-6 lg:h-6"
                     />
                   </a>
                 ))}
@@ -92,3 +216,160 @@ const AllMusic = () => {
 };
 
 export default AllMusic;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const Allmusic = () => {
+//   const songs = [
+//     {
+//       title: 'Aao Gan Raja',
+//       artist: 'Ft. Ishita Viswakarma',
+//       duration: '3:45',
+//       releaseDate: '05/01/24',
+//       platforms: {
+//         spotify: '/path-to-spotify-icon.png', // Replace with the actual path
+//         appleMusic: '/path-to-applemusic-icon.png',
+//         youtube: '/path-to-youtube-icon.png'
+//       }
+//     },
+//     {
+//       title: 'Aao Gan Raja',
+//       artist: 'Ft. Ishita Viswakarma',
+//       duration: '3:45',
+//       releaseDate: '05/01/24',
+//       platforms: {
+//         spotify: '/path-to-spotify-icon.png', // Replace with the actual path
+//         appleMusic: '/path-to-applemusic-icon.png',
+//         youtube: '/path-to-youtube-icon.png'
+//       }
+//     },
+//     {
+//       title: 'Aao Gan Raja',
+//       artist: 'Ft. Ishita Viswakarma',
+//       duration: '3:45',
+//       releaseDate: '05/01/24',
+//       platforms: {
+//         spotify: '/path-to-spotify-icon.png', // Replace with the actual path
+//         appleMusic: '/path-to-applemusic-icon.png',
+//         youtube: '/path-to-youtube-icon.png'
+//       }
+//     },
+//     {
+//       title: 'Aao Gan Raja',
+//       artist: 'Ft. Ishita Viswakarma',
+//       duration: '3:45',
+//       releaseDate: '05/01/24',
+//       platforms: {
+//         spotify: '/path-to-spotify-icon.png', // Replace with the actual path
+//         appleMusic: '/path-to-applemusic-icon.png',
+//         youtube: '/path-to-youtube-icon.png'
+//       }
+//     },
+//     {
+//       title: 'Aao Gan Raja',
+//       artist: 'Ft. Ishita Viswakarma',
+//       duration: '3:45',
+//       releaseDate: '05/01/24',
+//       platforms: {
+//         spotify: '/path-to-spotify-icon.png', // Replace with the actual path
+//         appleMusic: '/path-to-applemusic-icon.png',
+//         youtube: '/path-to-youtube-icon.png'
+//       }
+//     }
+//   ]
+
+//   return (
+//     <div className='min-h-screen bg-white flex justify-center items-start mt-16 py-16'>
+//       <div className='w-full lg:max-w-[62vw] max-w-[90vw] mx-auto '>
+//         {/* Filter Buttons */}
+//         <div className='mb-10'>
+//           <div className='lg:flex gap-8 lg:w-auto  border-2 lg:items-start lg:justify-between grid '>
+//             <button className='bg-red border-1 border-red text-white text-base font-semibold py-[9px] px-14 rounded-md'>
+//               All Music
+//             </button>
+//             <button className='border-2 border-red text-red text-base font-semibold py-2 px-14  rounded-md'>
+//               Devotional
+//             </button>
+//             <button className='border-2 border-red text-red text-base font-semibold py-2 px-14  rounded-md'>
+//               Play Back
+//             </button>
+//             <button className='border-2 border-red text-red text-base font-semibold py-2 px-14  rounded-md'>
+//               Original
+//             </button>
+//             <button className='border-2 border-red text-red text-base font-semibold py-2 px-14  rounded-md'>
+//               Jazz
+//             </button>
+//           </div>
+//         </div>
+
+//         {/* Song List */}
+//         <div className='flex flex-col gap-3'>
+//           {songs.map((song, index) => (
+//             <div
+//               key={index}
+//               className='bg-pink p-4 rounded-md grid lg:grid-cols-10  grid-cols-8 gap-5 align-middle  items-center lg:gap-2'
+//             >
+//               <h3 className='lg:text-base text-xl font-semibold lg:col-span-2 col-span-4'>
+//                 {song.title}
+//               </h3>
+//               <p className='lg:col-span-2  text-sm hidden lg:block '>
+//                 {song.artist}
+//               </p>
+//               <p className='col-span-2 lg:block hidden'>
+//                 Duration: {song.duration}
+//               </p>
+//               <p className='lg:col-span-2  lg:block hidden'>
+//                 Released On: {song.releaseDate}
+//               </p>
+//               <div
+//                 className='flex space-x-4 lg:items-end lg:col-span-2 col-span-4  lg:justify-center border-2 
+//              justify-end '
+//               >
+//                 <img
+//                   src='/ICONS/Icons/1x/spotify.png'
+//                   alt='Spotify'
+//                   className='lg:w-6 w-10 lg:h-6'
+//                 />
+//                 <img
+//                   src='/ICONS/Icons/1x/imusic.png'
+//                   alt='Apple Music'
+//                   className='lg:w-6 w-10 lg:h-6'
+//                 />
+//                 <img
+//                   src='/ICONS/Icons/1x/ytcolor.png'
+//                   alt='YouTube'
+//                   className='lg:w-6 w-10  lg:h-6'
+//                 />
+//               </div>
+//             </div>
+//           ))}
+//         </div>
+//       </div>
+//     </div>
+//   )
+// }
+
+// export default Allmusic
